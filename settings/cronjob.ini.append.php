b286dc4420e009c7049afbee3533917c6ceb1d73
Brookins Consulting <info@brookinsconsulting.com>
2014-09-30 12:36:42 -0500

    Updated: Moved current video sitemap generaetor (from objects) to it's own cronjob part

--- settings/cronjob.ini.append.php
+++ settings/cronjob.ini.append.php
@@ -4,8 +4,13 @@
 Scripts[]
 Scripts[]=bcgenerategooglevideositemap.php
 
+[CronjobPart-googlevideositemapfromobjects]
+Scripts[]
+Scripts[]=bcgenerategooglevideositemapfromobjects.php
+
 [CronjobSettings]
 Scripts[]=bcgenerategooglevideositemap.php
+Scripts[]=bcgenerategooglevideositemapfromobjects.php
 ExtensionDirectories[]=bcgooglevideositemap
 
 */ ?>
